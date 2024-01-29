c85f80b28f6ada8c34416bc0474cb302ba5bc53a
burcukilic <94201593+burcukilic@users.noreply.github.com>
2024-01-29 14:40:46 +0100

    Fixes issue/5 Created the cmvFunction1() that checks the condition1. Also wrote unit test for it in CMVTest.java

--- test/CMVTest.java
+++ test/CMVTest.java
@@ -18,7 +18,7 @@ public class CMVTest {
     
         points[8] = new Vector2D(3.1,3.1);
         
-        CMV cmv  = new CMV(points, 100, 2.0);
+        CMV cmv  = new CMV(points, 100, 2.0, 0);
 
         assertTrue(cmv.cmvFunction0());
     }
@@ -32,7 +32,7 @@ public class CMVTest {
             points[i] = new Vector2D(1.0,1.0);
         }
     
-        CMV cmv  = new CMV(points, 100, 2.0);
+        CMV cmv  = new CMV(points, 100, 2.0, 0);
 
         assertFalse(cmv.cmvFunction0());
     }
@@ -49,8 +49,57 @@ public class CMVTest {
         points[7] = new Vector2D(2.5,2.5);
         points[8] = new Vector2D(1.6,1.6);
         
-        CMV cmv  = new CMV(points, 100, 1.0);
+        CMV cmv  = new CMV(points, 100, 1.0, 0);
 
         assertTrue(cmv.cmvFunction0());
     }
+
+    @Test
+    public void testFunction1_0(){
+
+        Vector2D[] points = new Vector2D[100];
+
+        for(int i=0; i < 100; i++){
+            points[i] = new Vector2D(i,i);
+        }
+    
+                
+        CMV cmv  = new CMV(points, 10, 0, 1);
+
+        assertTrue(cmv.cmvFunction1());
+    }
+
+    @Test
+    public void testFunction1_1(){
+
+        Vector2D[] points = new Vector2D[100];
+
+        for(int i=0; i < 100; i++){
+            points[i] = new Vector2D(i,i);
+        }
+    
+                
+        CMV cmv  = new CMV(points, 10, 0, 2);
+
+        assertFalse(cmv.cmvFunction1());
+    }
+
+    @Test
+    public void testFunction1_2(){
+
+        Vector2D[] points = new Vector2D[100];
+
+        for(int i=0; i < 100; i++){
+            points[i] = new Vector2D(1,1);
+        }
+
+        points[1] = new Vector2D(4,1);
+        points[2] = new Vector2D(4,5);
+    
+                
+        CMV cmv  = new CMV(points, 10, 0, 5);
+
+        assertFalse(cmv.cmvFunction1());
+    }
+
 }
