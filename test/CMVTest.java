1e763af3f217ae037b8eb1eb5448a4304b64d5e1
Linus Wallin <linuswallin@live.se>
2024-01-30 08:26:51 +0100

    refactor: removed duplicate imports
    Removed 2 duplicate imports in CMVTest.

--- test/CMVTest.java
+++ test/CMVTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 
 public class CMVTest {
 
