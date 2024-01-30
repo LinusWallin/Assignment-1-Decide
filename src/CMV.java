3656bceb8b95de53b357bc7c8c993866a81c3ef9
Linus Wallin <linuswallin@live.se>
2024-01-30 06:22:06 +0100

    feat: implementation of function 7
    Implemented function 7 which looks at the difference in length
    between 2 points that are separeted by K_PTS consecutive points
    and compares it to LENGTH1.

--- src/CMV.java
+++ src/CMV.java
@@ -25,11 +25,12 @@ public class CMV {
     private int NUMPOINTS;
 
     //constructor for cmvFunction0
-    public CMV(Vector2D[] POINTS, int NUMPOINTS, double LENGTH1, double AREA1){
+    public CMV(Vector2D[] POINTS, int NUMPOINTS, double LENGTH1, double AREA1, int K_PTS){
         this.POINTS = POINTS;
         this.NUMPOINTS = NUMPOINTS;
         this.LENGTH1 = LENGTH1;
         this.AREA1 = AREA1;
+        this.K_PTS = K_PTS;
     }
     
     public boolean cmvFunction0(){
@@ -74,6 +75,20 @@ public class CMV {
                 double area = 0.5*Math.abs(det);
 
                 if (area > this.AREA1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean cmvFunction7(){
+
+        if (this.NUMPOINTS >= 3){
+
+            for (int i = 0; i < this.NUMPOINTS - (this.K_PTS+1); i++){
+
+                if (Math.sqrt(this.POINTS[i].squaredDistance(this.POINTS[i+this.K_PTS+1])) > this.LENGTH1){
                     return true;
                 }
             }
