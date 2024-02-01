fae4b4b0d643c257d6df695283b70f56415dc0f2
karlsb <36365664+karlsb@users.noreply.github.com>
2024-02-01 09:40:56 +0100

    Fix: changed mistake in update to main
    issue #11 added public variable in main by mistake

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.security.InvalidParameterException;
 
 class Main {
-    public double PI = 3.1415926535;
+    double PI = 3.1415926535;
     
     public static void main (String[] args) {
         System.out.println("This is the main file");
