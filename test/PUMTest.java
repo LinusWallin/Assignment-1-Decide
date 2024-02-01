952a1b7f21168154ea6bc511313646e1775e7afc
karlsb <36365664+karlsb@users.noreply.github.com>
2024-02-01 10:12:19 +0100

    Basic layout for PUM-test
    Towards issue #67
    
    Implement test based on this.

--- test/PUMTest.java
+++ test/PUMTest.java
@@ -7,5 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class PUMTest{
 
+    @Test
+    public void testPUMcalculatePUM_0(){
+        boolean[] cmvVector = new boolean[15];
+        OP[][] LCM = new OP[15][15];
+        
+        boolean[][] expected ={ 
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+            {true,true,true,true,true, true,true,true,true,true, true,true,true,true,true},
+        };
+
+
+        PUM pum = new PUM(cmvVector, LCM);
+        
+    }
 
 }
+
+
